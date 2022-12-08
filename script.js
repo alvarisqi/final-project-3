1dde7aff0b336799560f968dcb166ab16fc6fa62
alvarisqi <alvarisqi21@gmail.com>
2022-12-08 21:56:03 +0800

    update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".carousel-item").width();
-var scrollPosition = 0;
-$(".carousel-control-next").on("click", function () {
-    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-      scrollPosition += cardWidth;  //update scroll position
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-    }
-});
-$(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $(".carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-});
-var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    //rest of the code
-    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false
-    });
-  } else {
-    $(multipleCardCarousel).addClass("slide");
-  }
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
